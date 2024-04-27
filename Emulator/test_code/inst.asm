4f07a9761bd977959d0524c8c6b3acf835c30559
Your Name <you@example.com>
2024-04-27 17:29:26 +0800

    update inst 23~29 test

--- Emulator/test_code/inst.asm
+++ Emulator/test_code/inst.asm
@@ -1,34 +1,28 @@
-addi x16, x0, 0xfec
-addi x17, x0, 0x472
-addi x18, x0, 0xab7
-addi x19, x0, 0xda2
-addi x20, x0, 0x561
-addi x21, x0, 0x146
-addi x22, x0, 0x531
-addi x23, x0, 0x456
-addi x24, x0, 0x11
-addi x25, x0, 0x98
-addi x26, x0, 0xa3
-addi x27, x0, 0x10
-addi x28, x0, 0x55
-addi x29, x0, 0xffe
-addi x30, x0, 0x4
-addi x31, x0, 0xff1
-clz x1, x30
-cpop x2, x23
-xnor x3, x16, x17
-min x4, x31, x25
-maxu x5, x19, x30
-sext.b x6, x21
-bseti x7, x29, 0x0
-bclr x8, x31, x30
-binv x9, x16, x24
-bexti x10, x21, 0x2
-rol x11, x16, x30
-rori x12, x23, 0xc
-sh2add x13, x17, x18
-rev8 x14, x25
-orc.b x15, x25
+lui x20, 0x1234
+addi x20, x20, 0x567
+lui x21, 0x00000000
+addi x21, x21, 0x00000005
+lui x22, 0x00000000
+addi x22, x22, 0x00000003
+lui x23, 0x00000000
+addi x23, x23, 0x000000ff
+lui x24, 0x000ffff8
+addi x24, x24, 0x00000000
+lui x25, 0x00000100
+addi x25, x25, 0x00000100
+lui x26, 0x00010000
+addi x26, x26, 0x00000001
+rori x1, x20, 4
+rori x2, x20, 8
+rori x3, x20, 16
+sh1add x4, x21, x22
+sh2add x5, x21, x22
+sh3add x6, x21, x22
+rev8 x7, x20
+zext.h x8, x23
+zext.h x9, x24
+orc.b x10, x25
+orc.b x11, x26
 nop zero, zero, 0
 nop zero, zero, 0
 nop zero, zero, 0
